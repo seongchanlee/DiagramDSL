30bc012745c556bfe7b06a89c0eb3fc2889e3ff7
Mike's Macbook <mike@Mikes-MacBook-Pro.local>
2019-10-06 13:53:04 -0700

    initial node class setup with main

---
+++ src/libs/Node.java
@@ -0,0 +1,11 @@
+package libs;
+
+public abstract class Node {
+    // need to connect to renderer.
+
+    /*
+     * Abstract methods
+     */
+    abstract public void parse();
+    abstract public String evaluate();
+}
